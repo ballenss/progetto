070738ded180976c31cc75f3bb4c345c820d9649
Fily1212 <Fily1212>
2025-06-12 17:38:03 +0200

    profilo

---
+++ nuovo/profilo.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html lang="it">
+    
+    <head>
+<!-- includo header -->
+      <?php require 'sezioni/header.php' ?>
+      <title> Guess - Shop online</title>
+    </head>
+
+    <body>
+
+      <!-- includo navbar -->
+      <?php require 'sezioni/navbar.php' ?>
+
+       <article id="article_home">
+          <h1>Profilo <?php echo $_SESSION["nome"] ?></h1>
+         
+          <h3 id="identificatore_hover">Risparmia su una selezione di articoli</h3>
+
+          <section>
+              <h2 class="titoli_home"> Novità uomo </h2>
+              <img src="immagini/felpa-uomo.jpg" alt="felpa uomo" title="felpa uomo" 
+                     class="immagine-shop">
+              <img src="immagini/maglietta-uomo.jpg" alt="maglietta uomo" title="maglietta uomo" 
+                     class="immagine-shop">
+              <img src="immagini/jeans-uomo.jpg" alt="jeans uomo" title="jeans uomo" 
+                      class="immagine-shop">
+			</section>
+			
+          <section>
+              <h2 class="titoli_home"> Novità donna </h2>
+              <img src="immagini/felpa-donna.jpg" alt="felpa-donna" title="felpa-donna"
+                    class="immagine-shop">
+              <img src="immagini/maglietta-donna.jpg" alt="maglietta donna" title="maglietta donna" 
+                     class="immagine-shop">
+              <img src="immagini/jeans-donna.jpg" alt="jeans donna" title="jeans donna" 
+                     class="immagine-shop">
+          </section>
+		  
+          <section>
+              <h2 class="titoli_home"> Novità bambino </h2>
+              <img src="immagini/felpa-bambino.jpg" alt="felpa bambino" title="felpa bambino" 
+                    class="immagine-shop">
+              <img src="immagini/maglietta-bambino.jpg" alt="maglietta bambino" title="maglietta bambino" 
+                      class="immagine-shop">
+              <img src="immagini/jeans-bambino.jpg" alt="jeans bambino" title="jeans bambino" 
+                   class="immagine-shop">
+				   
+          </section>
+         
+
+       </article>
+       
+
+   <!-- includo footer -->
+
+ <?php require 'sezioni/footer.php' ?>
+
+    </body>
+
+</html>
